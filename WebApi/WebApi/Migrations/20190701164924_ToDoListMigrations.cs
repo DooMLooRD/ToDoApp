14bba2f7cdeed64cdac96f27d1235d51dc2e9e52
jakubkapuscinski <jakub.kapuscinski1997@gmail.com>
2019-07-01 18:55:01 +0200

    [Added] Model with migrations

---
+++ WebApi/WebApi/Migrations/20190701164924_ToDoListMigrations.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApi.Migrations
+{
+    public partial class ToDoListMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    taskId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: true),
+                    Decription = table.Column<string>(nullable: true),
+                    isDone = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.taskId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ WebApi/WebApi/Model/ToDoListContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Model
+{
+    public class ToDoListContext: DbContext
+    {
+        public ToDoListContext(DbContextOptions<ToDoListContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+
+    }
+}
