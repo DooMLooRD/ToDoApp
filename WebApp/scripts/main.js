45e30987c3b684958985bf779c4284e4f7b3c08f
jakubkapuscinski <jakub.kapuscinski1997@gmail.com>
2019-07-02 08:47:00 +0200

    [Edit] Separated deleteToDo to new method

--- WebApp/scripts/main.js
+++ WebApp/scripts/main.js
@@ -6,6 +6,9 @@ todoBtn.onclick = createToDo;
 loadToDos();
 
 function loadToDos() {
+  while (todos.firstChild) {
+    todos.removeChild(todos.firstChild);
+  }
   fetch("https://localhost:44325/api/Tasks")
     .then(resp => resp.json())
     .then(resp => {
@@ -27,12 +30,19 @@ function createToDo() {
   });
 }
 
+
+
+function deleteToDo(id) {
+  fetch("https://localhost:44325/api/RemoveTask?id=" + id, {
+    method: "delete"
+  }).then(() => loadToDos());
+}
 function addToDo(task) {
   if (task.taskId == null) {
     fetch("https://localhost:44325/api/AddTask", {
       method: 'POST',
       body: JSON.stringify(task),
-      headers: {'Content-Type': 'application/json'}
+      headers: { 'Content-Type': 'application/json' }
     })
       .then(res => res.json()).then(res => {
         task.taskId = res.taskId;
@@ -47,20 +57,18 @@ function addToDo(task) {
   todoIsDone.name = "todoIsDone";
   todoIsDone.value = task.isDone;
   todoIsDone.id = "todoIsDone";
-
+  todoIsDone.contentEditable = false;
+  var todoEditBtn = document.createElement("button");
+  todoEditBtn.textContent = "Edit";
+  todoEditBtn.onclick = function () { deleteToDo(task.taskId); }
   var todoRemoveBtn = document.createElement("button");
   todoRemoveBtn.textContent = "Delete";
-  todoRemoveBtn.onclick = function (e) {
-    todos.removeChild(todoItem);
-    fetch("https://localhost:44325/api/RemoveTask?id=" + task.taskId, {
-      method: "delete"
-    }).then(response => console.log(response));
-  };
-
+  todoRemoveBtn.onclick = function () { deleteToDo(task.taskId); }
   var todoItem = document.createElement("li");
   todoItem.appendChild(todoTitle);
   todoItem.appendChild(todoText);
   todoItem.appendChild(todoIsDone);
+  todoItem.appendChild(todoEditBtn);
   todoItem.appendChild(todoRemoveBtn);
 
   todos.appendChild(todoItem);
